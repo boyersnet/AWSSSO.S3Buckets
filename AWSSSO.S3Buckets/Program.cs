234e5362245f219ab252f63e39b12ad8fd950ef4
Chuck Boyer <git@boyersnet.com>
2021-05-19 09:12:54 -0400

    Adding project files and updating the readme with instructions.

---
+++ AWSSSO.S3Buckets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace AWSSSO.S3Buckets
+{
+    class Program
+    {
+        private static IAmazonS3 _s3Client;
+
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var options = builder.Build().GetAWSOptions();
+
+            _s3Client = options.CreateServiceClient<IAmazonS3>();
+            ListBuckets().Wait();
+        }
+
+        private static async Task ListBuckets()
+        {
+            ListBucketsRequest request = new ListBucketsRequest();
+            ListBucketsResponse response = await _s3Client.ListBucketsAsync(request);
+            // Process response.
+            foreach (S3Bucket bucket in response.Buckets)
+            {
+                Console.WriteLine($"{bucket.BucketName}");
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # AWSSSO.S3Buckets
+
+Demo project for using AWSSSO with .NET SDK.
+
+## Configuration
+1. Assumes an existing AWS Account
+2. Assumes there is at least 1 bucket within the AWS account.
+3. Configue AWS SSO
+
+```powershell
+aws configure sso
+```
+
+4. (Optional) Configuring SSO sometimes give an unfriendly profile name based on the permission set.  If you'd like to change the profile name to something more friendly, login to AWS using the AWS CLI v2.
+
+```powershell
+aws sso login --profile MyProfile
+```
+
+5. Configure the appsettings.json file with the profile
+
+```json
+{
+  "AWS": {
+    "Profile": "MyProfile",
+    "Region": "us-east-1"
+  }
+}
+```
+
+6. Run the project which should write the bucket names out to the console window.
